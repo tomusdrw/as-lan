7341ff5f53c7572111c853681a14af0d3b8f95fa
Tomasz Drwięga <tomusdrw@gmail.com>
2026-04-20 11:15:31 +0200

    pastebin: address Task 1 review follow-ups (asconfig format, unused import)

--- examples/pastebin/assembly/test-run.ts
+++ examples/pastebin/assembly/test-run.ts
@@ -1,4 +1,4 @@
-import { runTestSuites, TestSuite } from "@fluffylabs/as-lan/test";
+import { runTestSuites } from "@fluffylabs/as-lan/test";
 
 export function runAllTests(): void {
   runTestSuites([]);
